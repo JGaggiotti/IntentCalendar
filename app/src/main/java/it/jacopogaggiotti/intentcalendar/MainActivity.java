55d899e644a02c82c4572611fee45abdec9336c6
Jacopo <jacopogaggiotti@protonmail.com>
2023-04-30 12:29:07 +0200

    Testato l'utilizzo di un Intent per agiungere un'evento al calendario e utilizzata la classe Calendar per settare la data e ora + conversione con il metodo "getTimeInMillis()" in long che è stato utilizzato allinterno del "putExtra()" relativo alla EVENT_BEGIN e END _TIME.

--- app/src/main/java/it/jacopogaggiotti/intentcalendar/MainActivity.java
+++ app/src/main/java/it/jacopogaggiotti/intentcalendar/MainActivity.java
@@ -2,7 +2,12 @@ package it.jacopogaggiotti.intentcalendar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.view.View;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        View btn1 = findViewById(R.id.button1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Calendar calendar = Calendar.getInstance();
+                calendar.set(2023,Calendar.MAY,15,00,00);
+                long beginTime = calendar.getTimeInMillis();
+
+                Calendar calendar2 = Calendar.getInstance();
+                calendar2.set(2023,Calendar.MAY,15,23,59);
+                long endTime = calendar2.getTimeInMillis();
+
+                Intent intent = new Intent(Intent.ACTION_INSERT);
+                intent.setData(CalendarContract.Events.CONTENT_URI);
+                intent.putExtra(CalendarContract.Events.TITLE,"Festa dei CERI");
+                intent.putExtra(CalendarContract.Events.EVENT_LOCATION,"Gubbio");
+                intent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,beginTime);
+                intent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endTime);
+
+                if(intent.resolveActivity(getPackageManager()) != null){
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
